e28b48d6bb7b5c43f7ebe4ba34f61b35e43d12ba
okadarenta <marin111vv@gmail.com>
2023-08-30 17:30:23 +0900

    ログイン・ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :kanji_first_name, :kanji_last_name, :katakana_first_name, :katakana_last_name, :birthday])
   end
-
-  def basic_auth
+ def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
