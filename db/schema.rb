fd53f5910859501b19cec3b7b8e5a6c2f7d81eed
okadarenta <marin111vv@gmail.com>
2023-09-01 18:23:54 +0900

    ActiveHashを導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_084156) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_092126) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_084156) do
     t.integer "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
